1fbcdd99e451329496bec88b97b2703c541a4059
rohanraut16 <rraut8942@gmail.com>
2026-05-27 08:35:09 +0530

    Refactor user interface layout and styling

---
+++ script.js
@@ -0,0 +1,34 @@
+let userContainer=document.querySelector('.userContainer')
+
+const user=[
+    {
+        profileUrl:"img/model1.avif",
+        name:"somiya",
+        email:'ssomiya@somiya.com'
+    },
+    {
+        profileUrl:"img/model2.avif",
+        name:"Neena",
+        email:'neena@gupta.com'
+    },
+    {
+        profileUrl:"img/model6.avif",
+        name:"Raja",
+        email:'raja@singh.com'
+    },
+    {
+        profileUrl:"img/model3.avif",
+        name:"sansa",
+        email:'sansa@sans.com'
+    },
+    {
+        profileUrl:"img/model5.avif",
+        name:"john Abraham",
+        email:'John@abr.com'
+    },
+    {
+        profileUrl:"img/model4.avif",
+        name:"arya",
+        email:'sstark@arya.com'
+    },
+]
